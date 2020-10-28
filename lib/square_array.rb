0a797d203906fa3ff37ab3655e23673aab2009ab
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 05:06:47 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  array.length.times do |index|
+    array[index] = array[index] ** 2
+  end
+  array
 end
+
+# def square_array(array)
+#   while |index| < array.length do
+#     array[index] = array[index] ** 2
+#   end
+#   array
+# end
